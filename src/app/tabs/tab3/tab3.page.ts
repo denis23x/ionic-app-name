02acb8fc18013d7300b0605140ba4bc3a70c282d
denis23x <damage.23x@gmail.com>
2021-03-31 15:37:19 +0600

    update

--- src/app/tabs/tab3/tab3.page.ts
+++ src/app/tabs/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Plugins, CameraResultType } from '@capacitor/core';
+
+const { Camera } = Plugins;
 
 @Component({
   selector: 'app-tab3',
@@ -9,4 +12,17 @@ export class Tab3Page {
 
   constructor() {}
 
+  async takePicture() {
+    return await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+  }
+
+  clickCamera() {
+    this.takePicture().then(r => {
+      console.log(r);
+    });
+  }
 }
